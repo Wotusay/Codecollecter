e67305d462942941bf9b806a31da108f0fa7de26
Arthur_Robaeys <arthur.robaeys@student.howest.be>
2019-10-23 16:20:09 +0200

    modified

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import quotes from './assets/data/quotes.json';
 import './style.css';
 
 {
-    const init = () => {
-        console.log(quotes);
-        loadQuotes();
-    };
+  const init = () => {
+    console.log(quotes);
+    loadQuotes();
+  };
 
-    const loadQuotes = () => {
-        const $quotes = document.querySelector('.quotes');
-        quotes.quotes.forEach(quote => {
-            const $quote = document.createElement(`li`);
-            $quote.textContent = quote.quote;
-            $quotes.appendChild($quote);
-        });
-    }
+  const loadQuotes = () => {
+    const $quotes = document.querySelector('.quotes');
+    quotes.quotes.forEach(quote => {
+      const $quote = document.createElement(`li`);
+      $quote.textContent = quote.quote;
+      $quotes.appendChild($quote);
+    });
+  };
 
-    init();
+  init();
 }
