2660412561168e3c7a3481bc27ea0d71dd5d4ca4
wotusay <wout.salembier@hotmail.com>
2019-10-23 20:08:14 +0200

    alle foutjes weg

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import quotes from './assets/data/quotes.json';
 import './style.css';
 
 {
-    const init = () => {
-        console.log(quotes);
-        loadQuotes();
-    };
+  const init = () => {
+    console.log(quotes);
+    loadQuotes();
+  };
 
-    const loadQuotes = () => {
-        const $quotes = document.querySelector('.quotes');
-        $quotes.innerHTML = '';
-        quotes.quotes.forEach(quote => {
-            $quotes.innerHTML += 
+  const loadQuotes = () => {
+    const $quotes = document.querySelector('.quotes');
+    $quotes.innerHTML = '';
+    quotes.quotes.forEach(quote => {
+      $quotes.innerHTML +=
             `<div class="quote_wrapper">
         <li class="quote">${quote.quote}</li>
         <a class='quote-button' href="">more info
         </a>
       </div>`;
-        });
-    }
+    });
+  };
 
-    init();
+  init();
 }
