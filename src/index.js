5c2de8602e439e075d538d7b5dd3f1426c14d4e7
Lorik Konjusevci <loriku@Loriks-MacBook-Pro.local>
2019-10-23 16:15:41 +0200

    Quotes overview via JS

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
+import quotes from './assets/data/quotes.json';
 import './style.css';
 
 {
     const init = () => {
-        console.log(`test`);
-        //loadQuotes();
+        console.log(quotes);
+        loadQuotes();
     };
 
     const loadQuotes = () => {
-        $quotes = document.querySelector(`.quotes`);
-        const quotes = fetch('assets/data/quotes.json')
-            .then(response => {
-                return response.json();
-            });
-
-        $quotes.appendChild(`<li class="quote">${quotes[1].quote}</li>`);
+        const $quotes = document.querySelector('.quotes');
+        quotes.quotes.forEach(quote => {
+            const $quote = document.createElement(`li`);
+            $quote.textContent = quote.quote;
+            $quotes.appendChild($quote);
+        });
     }
 
     init();
